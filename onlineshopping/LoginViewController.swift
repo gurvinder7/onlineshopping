0be834d9001ee0ce3b35c03ddc0f1501753dbc95
Khushi Verma <khushiverma@khushis-macbook-pro.local>
2019-03-15 19:22:17 -0400

    added home screen

--- onlineshopping/LoginViewController.swift
+++ onlineshopping/LoginViewController.swift
@@ -1,24 +1,23 @@
 //
-//  ViewController.swift
+//  LoginViewController.swift
 //  onlineshopping
 //
-//  Created by gurvinder mangat on 2019-03-13.
+//  Created by Khushi Verma on 2019-03-15.
 //  Copyright © 2019 gurvinder mangat. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
+
+        // Do any additional setup after loading the view.
         self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: nil, action: nil)
     }
-
-    @IBAction func loginbtn(_ sender: UIButton) {
-        
+    
+    @IBAction func loginBtn(_ sender: UIButton) {
         let alertController = UIAlertController(title: "ALERT!", message: "UserName & Password is Incorrect", preferredStyle: .alert)
         
         let action1 = UIAlertAction(title: "Ok", style: .default)
@@ -29,6 +28,6 @@ class ViewController: UIViewController {
         self.present(alertController, animated: true)
     }
     
+   
 
 }
-
